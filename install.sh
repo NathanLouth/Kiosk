907cda3c8d6ddd53e27a44535c9137cd60aea339
Nathan Louth <77612558+NathanLouth@users.noreply.github.com>
2025-08-24 17:14:00 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -72,6 +72,15 @@ while [[ $# -gt 0 ]]; do
             SCREEN_RESOLUTION="$2"
             shift 2
             ;;
+
+        --auto-refresh)
+            if ! [[ "$2" =~ ^[0-9]+$ ]]; then
+                echo "Error: The --auto-refresh option requires a numeric value (in seconds) to specify the refresh interval." >&2
+                exit 1
+            fi
+            REFRESHSEC="$2"
+            shift 2
+            ;;
             
         --incognito)
             BROWSER_FLAGS="$BROWSER_FLAGS --incognito"
@@ -279,6 +288,29 @@ fi
 cat > /etc/sudoers.d/kiosk-reboot <<EOL
 kiosk ALL=(ALL) NOPASSWD:/usr/bin/systemctl reboot
 EOL
+
+if [ -n "$REFRESHSEC" ]; then
+# Install wtype to send keystrokes
+apt install wtype -y
+
+# Create auto-refresh service
+cat > /etc/systemd/system/kiosk-auto-refresh.service <<EOL
+[Unit]
+Description=Auto refresh chrome
+After=multi-user.target
+
+[Service]
+ExecStart=/usr/bin/wtype -M ctrl -k F5 -m ctrl
+Restart=always
+RestartSec=${REFRESHSEC}
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+# Enable refresh service
+systemctl enable kiosk-auto-refresh.service
+fi
 
 # Create and enable firewall (nftables)
 cat > /etc/nftables.conf <<EOL
